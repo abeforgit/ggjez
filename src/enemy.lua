82d5c5f992b3085da3108480e6fdacb2d306ff19
Robbe Van Herck <robbevanherck1@gmail.com>
2019-01-26 15:06:46 +0100

    fix merge

--- src/enemy.lua
+++ src/enemy.lua
@@ -1,16 +1,16 @@
-Utils = require("src.utils")
-Actor = require("src.actor")
+local Utils = require("src.utils")
+local Actor = require("src.actor")
 
 Enemy = Utils.inheritsFrom(Actor)
 
-function Enemy:new(x, y, player, imagePath)
+function Enemy:new(x, y, target, imagePath)
   local enemy = self.create()
-  self.image = love.graphics.newImage(imagePath)
+  self.img = love.graphics.newImage(imagePath)
   self.x = x
   self.y = y
   self.speed = 100
   self.health = 100
-  self.target = player
+  self.target = target
   return enemy
 end
 
@@ -22,18 +22,16 @@ function Enemy:update(dt)
   local dx = self.target.x - self.x
   local dy = self.target.y - self.y
   local length = math.sqrt(dx*dx + dy*dy)
-  local targetX = self.x + (dx / length) * dt * self.speed
-  local targetY = self.y + (dy / length) * dt * self.speed
-  local playerFilter = function(item, other)
-    return "bounce"
+  if length ~= 0 then
+    local targetX = self.x + (dx / length) * dt * self.speed
+    local targetY = self.y + (dy / length) * dt * self.speed
+    local playerFilter = function(item, other)
+      return "bounce"
+    end
+    local newX, newY, cols, len = self.scene.world:move(self, targetX, targetY, playerFilter)
+    self.x = newX
+    self.y = newY
   end
-  local newX, newY, cols, len = self.scene.world:move(self, targetX, targetY, playerFilter)
-  self.x = newX
-  self.y = newY
-end
-
-function Enemy:draw() 
-  love.graphics.draw(self.image, self.x, self.y)
 end
 
 return Enemy
