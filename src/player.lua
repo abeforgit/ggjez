deb64b5eedbfba719e959ec48195af8116d945be
abeforgit <arnebertrand@gmail.com>
2019-01-26 15:13:23 +0100

    added hump for vectors

--- src/player.lua
+++ src/player.lua
@@ -1,5 +1,6 @@
 util = require("src.utils")
 Actor = require("src.actor")
+Vector = require("lib.hump.vector")
 
 local Player = util.inheritsFrom(Actor)
 
@@ -10,12 +11,12 @@ Player.health = 100
 Player.xVel = 0
 Player.yVel = 0
 Player.speed = spd
-Player.moveVec = nil
+Player.moveVec = Vector.new()
 Player.vecs = {
-    up = {x = 0, y = -spd},
-    down = {x = 0, y = spd},
-    left = {x = -spd, y = 0},
-    right = {x = spd, y = 0}
+    up = Vector.new(0, -spd),
+    down = Vector.new(0, spd),
+    left = Vector.new(-spd, 0),
+    right = Vector.new(spd, 0)
 }
 function Player:new()
     plr = self.create()
@@ -24,7 +25,7 @@ function Player:new()
 end
 
 function Player:update(dt)
-    if self.moveVec ~= nil then
+    if self.moveVec ~= Vector.new(0, 0) then
         actualX, actualY, cols, len = self.scene.world:move(self, self.x + self.moveVec.x * dt, self.y + self.moveVec.y * dt, 
         function(item, other)
             return "cross"
@@ -48,13 +49,13 @@ end
 function Player:keypressed()
     return function (key, scancode, isrepeat)
         if util.upKeys[key] then
-            self.moveVec = self.vecs.up
+            self.moveVec = self.moveVec + self.vecs.up
         elseif util.downKeys[key] then
-            self.moveVec = self.vecs.down
+            self.moveVec = self.moveVec + self.vecs.down
         elseif util.leftKeys[key] then
-            self.moveVec = self.vecs.left
+            self.moveVec = self.moveVec + self.vecs.left
         elseif util.rightKeys[key] then
-            self.moveVec = self.vecs.right
+            self.moveVec = self.moveVec + self.vecs.right
         end
     end 
 end
@@ -62,7 +63,15 @@ end
 
 function Player:keyreleased(key, scancode, isrepeat)
     return function (key, scancode, isrepeat)
-                self.moveVec = nil
+        if util.upKeys[key] then
+            self.moveVec = self.moveVec - self.vecs.up
+        elseif util.downKeys[key] then
+            self.moveVec = self.moveVec - self.vecs.down
+        elseif util.leftKeys[key] then
+            self.moveVec = self.moveVec - self.vecs.left
+        elseif util.rightKeys[key] then
+            self.moveVec = self.moveVec - self.vecs.right
+        end
     end
 end
 
